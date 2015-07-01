34f7a23ced37247858a9e25f12355fc9c66f3d3e
ckaeser <christian.kaeser@businessdecision.com>
2015-07-01 11:11:36 +0200

    Employee View

---
+++ EmployeeBook.WebClientRazorAndJQuery/EmployeeBook.WebClientRazorAndJQuery/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace EmployeeBook.WebClientRazorAndJQuery.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
